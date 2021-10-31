75022fc661924d31306db2e5d838f26fc90db017
leomoyano <leomoyano7@gmail.com>
2021-10-31 16:29:10 -0300

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Button, Menu, Typography, Avatar } from 'antd';
+import { Link } from 'react-router-dom';
+import { HomeOutlined, MoneyCollectedOutlined, BalbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
+import icon from '../images/cryptocurrency.png'
 
 const Navbar = () => {
     return (
-        <div>
-            
+        <div className="nav-container">
+            <div className="logo-container">
+                <Avatar src={icon} size="large"/>
+                <Typography.Title level={2} className="log">
+                    <Link to="/">CryptoApp</Link>
+                </Typography.Title>
+                {/* <Button className="menu-control-container">
+
+                </Button> */}
+            </div>
         </div>
     )
 }
