2d2a94914cabfa2d217a9760472535bc8d90ef7d
leomoyano <leomoyano7@gmail.com>
2021-11-07 16:17:30 -0300

    update

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const CryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': 'a619efdd92mshf7e22cb84a32fabp1b9877jsn496116fddf65'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com/stats';
+
+const createRequest = (url) => ({url, headers: CryptoApiHeaders})
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl}),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () =>  createRequest('/coins')
+        })
+    })
+});
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
