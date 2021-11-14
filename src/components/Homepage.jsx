4156832da23b53657b84c60a9914ae61fa8e5a82
leomoyano <leomoyano7@gmail.com>
2021-11-14 16:41:29 -0300

    develop in progress - homepage heading containers

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,6 +3,7 @@ import millify from 'millify';
 import { Typography, Row, Col, Statics, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
 import { useGetCryptosQuery } from '../services/cryptoApi';
+import { Cryptocurrencies } from '.';
 const { Title } = Typography;
 
 const Homepage = () => {
@@ -21,6 +22,16 @@ const Homepage = () => {
                 <Col span={12}><Statistic title="Total 24Hs Volume" value={millify(globalStats.total24hVolume)} /></Col>
                 <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} /></Col>
             </Row>
+            <div className="home-heading-container">
+                <Title level={2} className="home-title">Top 10 Cryptocurrencies in the world</Title>
+                <Title level={3} className="show-more"><Link to="/criptocurrencies">Show More</Link></Title>
+            </div>
+            <Cryptocurrencies />
+            <div className="home-heading-container">
+                <Title level={2} className="home-title">Latest Crypto News</Title>
+                <Title level={3} className="show-more"><Link to="/news">Show More</Link></Title>
+            </div>
+            
         </>
     )
 }
